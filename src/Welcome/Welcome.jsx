9ee485fcfdb761df84b25e0618de42e496f85892
aryannkaushikk <aryan.kaushik2015@gmail.com>
2025-01-26 20:15:40 +0530

    CSS changes

--- src/Welcome/Welcome.jsx
+++ src/Welcome/Welcome.jsx
@@ -1,10 +1,64 @@
 import React, { useRef, useState, useEffect } from "react";
 import NavBar from "../NavBar/NavBar";
-import { motion, useInView } from "framer-motion";
+import { color, motion, useInView } from "framer-motion";
 import NumberFlow from '@number-flow/react'
 
 
 export default function Welcome() {
+
+  const infoDiv = {
+  marginTop: "10vmin",
+  marginLeft: "6vmin",
+  borderTop: "6px red solid",
+  display: "flex",
+  justifyContent: "start",
+  flexDirection: "column",
+  padding: "0% 1.5% 0% 1.5%",
+  color: "white",
+  width: "21vw",
+  flexGrow: "1"
+  }
+
+  const infoH1 = {
+    fontSize: "13.2vmin",
+  /* border: 1px pink solid; */
+  color: "red"
+  };
+
+  const infoH4 = {
+    fontSize: "4.5vmin"
+  };
+
+  const infoH6 = {
+    fontSize: "2.1vmin",
+  fontWeight: "100"
+  };
+
+  const headlineDiv = {
+    // border: "3px white solid",
+    width: "93vw",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "end",
+  }
+
+  const headLineH1 = {display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap",
+    color: "white"};
+
+  const headLineH1Span = {
+    fontSize: "14.1vmin",
+    textAlign: "center",
+    /* border: 1px white solid; */
+    width: "fit-content",
+    margin: "0px 12px 0px 12px"
+  };
+  
+
+
+
   const [number, setNumber] = useState(0);
   const [isAnimating, setIsAnimating] = useState(true);
   const [buttonPressed, setButtonPressed] = useState(false);
@@ -51,9 +105,9 @@ export default function Welcome() {
         initial={{ opacity: 0 }}
         animate={{opacity:isInView?1:0}}
         transition={{ duration: 2}}
-        className="welcome-elements"
+        style={{...headlineDiv,gridColumn:"1/4",gridRow:"1/2"}}
       >
-        <h1> {text.map((el, i) => (
+        <h1 style={headLineH1}> {text.map((el, i) => (
         <motion.span
           initial={{ opacity: 0 }}
           animate={{ opacity: isInView?1:0 }}
@@ -62,40 +116,46 @@ export default function Welcome() {
             delay: i / 5  ,
           }}
           key={i}
+          style={headLineH1Span}
         >
           {el}{" "}
         </motion.span>
       ))}</h1>
       </motion.div>
-      <div className="welcome-elements">
+      
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: isInView? 1:0, y: 0 }}
           transition={{ duration: 2 }}
+          style={{...infoDiv, gridColumn:"1/2", gridRow:"2/3"}}
+          className="welcome-elements" 
         >
-          <h1>{isAnimating ? <NumberFlow value={number} /> : <NumberFlow value={90} />}</h1>
-          <h4>Mins Service</h4>
-          <h6>Time is precious, don't waste it at workshops</h6>
+          <h1 style={infoH1}>{isAnimating ? <NumberFlow value={number} /> : <NumberFlow value={90} />}</h1>
+          <h4 style={infoH4}>Mins Service</h4>
+          <h6 style={infoH6}>Time is precious, don't waste it at workshops</h6>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: isInView? 1:0, y: 0 }}
                     transition={{ duration: 2 }}
+                    style={{...infoDiv,gridColumn:"2/3"}}
+                    className="welcome-elements" 
         >
-          <h1>{isAnimating ? <NumberFlow value={(Math.ceil(number*8/100))+number} /> : <NumberFlow value={11} />}+</h1>
-          <h4>Years of Experience</h4>
-          <h6>Team of experienced mechanics</h6>
+          <h1 style={infoH1}>{isAnimating ? <NumberFlow value={(Math.ceil(number*8/100))+number} /> : <NumberFlow value={11} />}+</h1>
+          <h4 style={infoH4}>Years of Experience</h4>
+          <h6 style={infoH6}>Team of experienced mechanics</h6>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: isInView? 1:0, y: 0 }}
           transition={{ duration: 2 }}
+          style={{...infoDiv, gridColumn:"3/4"}}
+          className="welcome-elements" 
         >
-          <h1>{isAnimating ? <NumberFlow value={Math.ceil(((number*8*1000)+(number*7*10)+number)/1000)} /> : <NumberFlow value={1000} />}</h1>
-          <h4>Kms Warranty</h4>
-          <h6>1 month or 1000 kms unconditional warranty</h6>
+          <h1 style={infoH1}>{isAnimating ? <NumberFlow value={Math.ceil(((number*8*1000)+(number*7*10)+number)/1000)} /> : <NumberFlow value={1000} />}</h1>
+          <h4 style={infoH4}>Kms Warranty</h4>
+          <h6 style={infoH6}>1 month or 1000 kms unconditional warranty</h6>
         </motion.div>
-      </div>
     </div>
   );
 }
